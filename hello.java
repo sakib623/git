47314cca1f89e0c498f90a7c6d61781ba4c08dfe
sakib623 <sakibal623@gmail.com>
2020-07-27 11:27:07 +0600

    add new method sayHello

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class Hello{
     public static void Main(final String[] args) {
                 System.out.println("This is my first code");
+
+                sayHello("Sakib");
+    }
+
+    public static void sayHello(String name){
+        System.out.println("Hello "+name);
     }
 }
